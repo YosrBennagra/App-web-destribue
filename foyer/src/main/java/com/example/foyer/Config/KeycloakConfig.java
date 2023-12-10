d39ca8b7f58e051bd3fab8855ef8571501fbfad1
nejdbedoui <nejdbedoui@gmail.com>
2023-12-10 17:13:25 +0100

    keyclock

---
+++ foyer/src/main/java/com/example/foyer/Config/KeycloakConfig.java
@@ -0,0 +1,60 @@
+package com.example.foyer.Config;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    static Keycloak keycloak=null;
+     final static String serverUrl = "http://localhost:8081/auth";
+    public final static String realm = "JobKeyClock";
+    public final static String clientId = "foyer-service";
+     final static String clientSecret = "CCah7FFSHwHsqY9H7UOHZzDn8DvYTMoJ";
+    final static String userName = "admin";
+    final static String password = "123456789";
+
+    public KeycloakConfig() {
+    }
+@Bean
+    public static Keycloak getInstance() {
+        if (keycloak == null) {
+            keycloak = KeycloakBuilder.builder()
+                    .serverUrl(serverUrl)
+                    .realm(realm)
+                    .grantType(OAuth2Constants.PASSWORD)
+                    .username(userName)
+                    .password(password)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .resteasyClient(new ResteasyClientBuilder()
+                            .connectionPoolSize(10)
+                            .build())
+                    .build();
+        }
+    return keycloak;
+}
+
+
+
+
+
+
+
+}
+
+
+
