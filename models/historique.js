6efd220104f0a79a86fc212f13769556f8119197
Jallouli-Yassine <jungloul1@gmail.com>
2023-12-11 16:38:33 +0100

    connect to another DB (sql)

---
+++ models/historique.js
@@ -0,0 +1,23 @@
+/*const mongoose = require("mongoose");
+const universiteSchema = new mongoose.Schema(
+    {
+      nom: {
+        type: String,
+        require: true,
+      },
+      description: {
+        type: String,
+        require: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+
+    }
+  );
+
+  const Universite = mongoose.model("universite", universiteSchema);
+
+module.exports = Universite;
+*/
