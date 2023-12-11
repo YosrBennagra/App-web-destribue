32aa12bd47abc793b74dcb9389c6931f3ec33e3d
Jallouli-Yassine <jungloul1@gmail.com>
2023-12-11 17:00:11 +0100

    add getOne and getAll histo apis

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ app.use(cors());
 
 //controllers : 
 const universiteController = require('./controllers/universiteContoller');
+const historiqueController = require('./controllers/historiqueController');
 
 
 app.use(bodyParser.json());
 
 app.use('/universite', universiteController);
+app.use('/historique', historiqueController);
 
 // Start the server
 const port = 3000;
