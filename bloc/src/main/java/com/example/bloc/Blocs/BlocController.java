b3373380056893566e1b055597798ccaa9c6302c
YosrBennagra <80179447+YosrBennagra@users.noreply.github.com>
2023-12-11 16:01:45 +0100

    Update 2

--- bloc/src/main/java/com/example/bloc/Blocs/BlocController.java
+++ bloc/src/main/java/com/example/bloc/Blocs/BlocController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/bloc")
 @RestController
+@CrossOrigin(origins = "*")
 
 public class BlocController {
     private final BlocService blocService;
